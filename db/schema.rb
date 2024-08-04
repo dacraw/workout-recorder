ba8979166c7d65444e916fa7ce7bcc243ae2ab4a
Doug <doug.a.crawford@gmail.com>
2024-08-04 12:51:03 -0700

    Migrate MuscleGroup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_04_193900) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_04_194927) do
+  create_table "muscle_groups", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "workouts", force: :cascade do |t|
     t.string "name"
     t.string "description"
