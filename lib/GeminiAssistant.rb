cee978eb54e96ae1b0dc727483cab8b4de66c31d
Doug <doug.a.crawford@gmail.com>
2024-08-11 02:23:18 -0700

    Add GeminiAssistant module

--- lib/GeminiAssistant.rb
+++ lib/GeminiAssistant.rb
@@ -1,9 +1,20 @@
-
-
 module GeminiAssistant
-    def hello
+    def evaluate_workout(workout_id)
         bot = NanoBot.new(cartridge: CARTRIDGE_CONFIG)
 
-        puts bot.eval('Hello')
+        workout = Workout.find_by id: workout_id
+
+        if !workout.present?
+            throw "No workout found"
+        end
+
+        workout_name = workout.name
+        workout_description = workout.description
+
+        bot.eval(<<-HEREDOC
+            workout name: #{workout_name}
+            workout description: #{workout_description}
+        HEREDOC
+        )
     end
 end
