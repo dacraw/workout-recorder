1e655ab8dbc8f9cad6c60b48e8c0867204be43e1
Douglas Crawford <59629330+dacraw@users.noreply.github.com>
2024-08-13 01:20:48 -0700

    Implement gem assist (#13)
    * Add gemini response column to workouts table
    
    * Render gemini improvement/analysis for each workout
    
    * Set gemini response in index if necessary and alter response to provide analysis first
    
    * Add white space and borders to workout index
    
    * Set Gemini response before updating workout model

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,26 @@
 class Workout < ApplicationRecord
+    include GeminiAssistant
+    
     belongs_to :muscle_group
+
+    # TODO: Only call this is name/description field changes to minimize Google API calls
+    before_save :set_gemini_response
+
+    def gemini_response_html
+        markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+        markdown.render self.gemini_response
+    end
+
+    def get_and_set_gemini_response_html
+        response = evaluate_workout_name_and_description self.name, self.description
+        self.update_column :gemini_response, response
+
+        gemini_response_html
+    end
+
+    private
+
+    def set_gemini_response
+        self.gemini_response = evaluate_workout_name_and_description self.name, self.description
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_05_044616) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_11_093040) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_05_044616) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "muscle_group_id"
+    t.text "gemini_response"
     t.index ["muscle_group_id"], name: "index_workouts_on_muscle_group_id"
   end
 
