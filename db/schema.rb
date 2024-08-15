fcebd5ac3c0db292e291ccd8e6d80a36edc22527
Doug <doug.a.crawford@gmail.com>
2024-08-14 17:58:42 -0700

    Add workouts table and create foreign key relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_14_212837) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_15_005554) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_14_212837) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "gemini_response"
+    t.bigint "workout_id"
+    t.index ["workout_id"], name: "index_exercises_on_workout_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_14_212837) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "workouts", force: :cascade do |t|
+    t.date "date"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_workouts_on_user_id"
+  end
+
+  add_foreign_key "exercises", "workouts"
 end
