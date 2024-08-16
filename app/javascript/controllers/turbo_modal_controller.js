9e940c57a495a12f163e6d708daea07194732b9b
Doug <doug.a.crawford@gmail.com>
2024-08-15 22:11:46 -0700

    Hide modal when clicking outside

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -2,8 +2,23 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["modal"];
+
   hideModal() {
     this.element.parentElement.removeAttribute("src");
     this.element.remove();
+  }
+
+  submitEnd(e) {
+    if (e.detail.success) {
+      this.hideModal();
+    }
+  }
+
+  closeBackground(e) {
+    if (e && this.modalTarget.contains(e.target)) {
+      return;
+    }
+    this.hideModal();
   }
 }
