363c7d4b265831625a60d53645b93d4ea1a8ecd4
Doug <doug.a.crawford@gmail.com>
2024-08-16 18:37:14 -0700

    Apply active workout class when selecting different workout

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -8,8 +8,28 @@ export default class extends Controller {
   }
 
   toggleAccordion(e) {
-    const target = e.target.closest("span").nextElementSibling;
-    target.classList.toggle("hidden");
+    // Remove `active-workout` class from elements that were part of the previously selected workout
+    const previouslyActive = document.querySelectorAll(".active-workout");
+    previouslyActive.forEach((target) =>
+      target.classList.remove("active-workout")
+    );
+
+    // Target the element containing the workout name as the base for toggling exercise list and `active-workout` class
+    const workoutNameElement = e.target.closest("span");
+    const workoutExercisesElement = workoutNameElement.nextElementSibling;
+
+    workoutNameElement.classList.add("active-workout");
+    workoutExercisesElement.classList.add("active-workout");
+    workoutExercisesElement.classList.toggle("hidden");
+  }
+
+  toggleActiveWorkout(target) {
+    const previouslyActive = document.querySelectorAll(".active-workout");
+    previouslyActive.forEach((target) =>
+      target.classList.remove("active-workout")
+    );
+
+    target.classList.add("active-workout");
   }
 
   connect() {}
