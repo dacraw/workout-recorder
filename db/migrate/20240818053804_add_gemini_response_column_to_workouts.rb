bef8ba3ae24daadeffa129076f3e7ed546c811df
Doug <doug.a.crawford@gmail.com>
2024-08-17 22:39:02 -0700

    Create gemini_response column on workouts table

---
+++ db/migrate/20240818053804_add_gemini_response_column_to_workouts.rb
@@ -0,0 +1,5 @@
+class AddGeminiResponseColumnToWorkouts < ActiveRecord::Migration[7.1]
+  def change
+    add_column :workouts, :gemini_response, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_17_045023) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_18_053804) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_17_045023) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "gemini_response"
     t.index ["user_id"], name: "index_workouts_on_user_id"
   end
 
