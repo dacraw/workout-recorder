8f50992adf68fae4b87ac37714955b4385d940e5
Doug <doug.a.crawford@gmail.com>
2024-08-23 13:27:00 -0700

    Allow anyone to view workout index page

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,7 +1,7 @@
 include GeminiAssistant
 
 class WorkoutsController < ApplicationController
-    before_action :authenticate_user!
+    before_action :authenticate_user!, except: %i[ index ]
     before_action :set_workout, only: %i[ show destroy evaluate_workout ]
     before_action :check_author, only: :destroy
 
