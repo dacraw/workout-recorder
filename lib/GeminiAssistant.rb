5d98f803401351c26e902bb8010c6e6dda1d29df
Doug <doug.a.crawford@gmail.com>
2024-08-23 22:38:51 -0700

    Rescue StandardError from allegedly unsafe evaluations

--- lib/GeminiAssistant.rb
+++ lib/GeminiAssistant.rb
@@ -1,43 +1,26 @@
 module GeminiAssistant
-    def evaluate_exercise(exercise_id)
-        bot = NanoBot.new(cartridge: CARTRIDGE_CONFIG)
-
-        exercise = Exercise.find_by id: exercise_id
-
-        if !exercise.present?
-            throw "No exercise found"
-        end
-
-        exercise_name = exercise.name
-        exercise_description = exercise.description
-
-        bot.eval(<<-HEREDOC
-            Provide a one sentence analysis and one sentence improvement for the following exercise:
-
-            exercise name: #{exercise_name}
-            exercise description: #{exercise_description}
-        HEREDOC
-        )
-    end
-
     def evaluate_workout(workout_id)
-        bot = NanoBot.new(cartridge: CARTRIDGE_CONFIG)
+        begin
+            bot = NanoBot.new(cartridge: CARTRIDGE_CONFIG)
 
-        workout = Workout.includes(:exercises).find_by(id: workout_id)
-        exercises = workout.exercises
+            workout = Workout.includes(:exercises).find_by(id: workout_id)
+            exercises = workout.exercises
 
-        exercise_text = exercises.map {|exercise| "exercise name: #{exercise.name}, exercise description: #{exercise.description}"}.join(";")
+            exercise_text = exercises.map {|exercise| "exercise name: #{exercise.name}, exercise description: #{exercise.description}"}.join(";")
 
-        heredoc_text = <<-HEREDOC
-        Provide a one paragraph analysis of the following workout, which contains these exercises:
-            #{exercise_text}
+            heredoc_text = <<-HEREDOC
+            Provide a one paragraph analysis of the following workout, which contains these exercises:
+                #{exercise_text}
 
-        HEREDOC
+            HEREDOC
 
-        return "Cannot analyze this workout as it does not exist" if workout.blank?
+            return "Cannot analyze this workout as it does not exist" if workout.blank?
 
-        bot.eval heredoc_text
-            
+            bot.eval heredoc_text
+        rescue StandardError => e
+            puts e
+            "There was a safety issue with your workout information. As long as you are using safe language, try re-evaluating the workout since sometimes the chatbot misunderstands."
+        end
     end
 
     def evaluate_exercise_name_and_description(name, description)
@@ -46,15 +29,21 @@ module GeminiAssistant
             return
         end
 
-        bot = NanoBot.new(cartridge: CARTRIDGE_CONFIG)
+        begin
 
-        bot.eval(
-            <<-HEREDOC
-                Provide a one sentence analysis and one sentence improvement for the following exercise:
-                
-                exercise name: #{name}
-                exercise description: #{description}
-            HEREDOC
-        )
+            bot = NanoBot.new(cartridge: CARTRIDGE_CONFIG)
+
+            bot.eval(
+                <<-HEREDOC
+                    Provide a one sentence analysis and one sentence improvement for the following exercise:
+                    
+                    exercise name: #{name}
+                    exercise description: #{description}
+                HEREDOC
+            )
+        rescue StandardError => e
+            puts e
+            "There was a safety issue evaluating the exercise - please ensure you are using appropriate language and try editing then saving the exercise to re-generate an analysis."
+        end
     end
 end
