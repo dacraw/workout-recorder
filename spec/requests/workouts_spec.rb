e3b290813fac5c7175198a3944b9ec3c6b86fbcf
Doug <doug.a.crawford@gmail.com>
2024-09-01 20:34:37 -0700

    Add controller spec for workout create

--- spec/requests/workouts_spec.rb
+++ spec/requests/workouts_spec.rb
@@ -9,6 +9,31 @@ RSpec.describe "Workouts", type: :request do
       get workouts_path
 
       expect(response).to render_template :index
+      expect(response.body).to include("User " + workout_with_exercises.user.id.to_s)
+      expect(response.body).not_to include("User " + workout_without_exercises.user.id.to_s)
+    end
+  end
+
+  describe "POST /create" do
+    let(:user) { create :user }
+
+    before :each do
+      sign_in user
+    end
+
+    it "creates a new workout" do
+      expect {
+        post workouts_path
+      }.to change { Workout.count }.by 1
+    end
+
+    it "redirects to the My Workouts page with the new workout's id as a param" do
+      post workouts_path
+
+      expect(response).to redirect_to url_for(controller: 'workouts', action: 'my_workouts', params: {workout_id: Workout.last.id})    
+
+      follow_redirect!
+      expect(response.body).to include '<h1 class="page-header">My Workouts</h1>'
     end
   end
 end
