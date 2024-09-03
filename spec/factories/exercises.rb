41cd5e685310132ccaa2252763592638125d2803
Doug <doug.a.crawford@gmail.com>
2024-09-03 15:24:42 -0700

    Add description trait to exercises factory

--- spec/factories/exercises.rb
+++ spec/factories/exercises.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :exercise do
     name { Faker::Lorem.word }
     workout
+
+    trait :with_a_description do
+      description { Faker::Lorem.sentence }
+    end
   end
 end
