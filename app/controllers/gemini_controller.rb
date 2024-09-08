4f9cc82f195539403a6361e5593be5e2c2d7997b
Doug <doug.a.crawford@gmail.com>
2024-09-07 22:43:42 -0700

    Add suggest workout

--- app/controllers/gemini_controller.rb
+++ app/controllers/gemini_controller.rb
@@ -2,12 +2,17 @@ class GeminiController < ApplicationController
     before_action :authenticate_user!
 
     def tag_selector
-
     end
 
     def suggest_exercises
 
     end
 
-
+    def suggest_workout
+        response = GeminiAssistant.suggest_workout_based_on_type(params[:prompt])
+        @selected_tags = params[:prompt]
+        
+        rc = Redcarpet::Markdown.new(Redcarpet::Render::HTML, hard_wrap: true)
+        @html_response = rc.render response
+    end
 end
